a4d107109eb5add3190cafd5a35811b65db85c47
zackzachariah <zack@zacksheppard.com>
2019-01-30 21:10:45 +0000

    Improve styling on social links

--- js/content/HomeWelcome.tsx
+++ js/content/HomeWelcome.tsx
@@ -18,38 +18,42 @@ const HomeWelcome = () => (
       <p>
         <strong>
           I&rsquo;m an engineer with over 10 years experience making server,
-          web, and iOS products.
+          web, and iOS products. These days I'm a freelancer and consultant.
         </strong>
       </p>
 
       <p>I&rsquo;ve also bartended, been a manager, and love to travel.</p>
 
       <p>
-        I&rsquo;m currently available as a freelancer or consultant. If
-        you&rsquo;d like to work with me or just grab coffee and chat, say hi.
+        I&rsquo;m currently based in New York City. If you&rsquo;d like to work
+        with me or just grab coffee and chat, say hi:
       </p>
     </div>
     <div id="social">
-      <a className="fa-link" href="mailto:contact@zacksheppard.com">
-        <span className="sr-only">Email Zack</span>
-        <FontAwesomeIcon icon={faEnvelopeOpenText} />
-      </a>
-      <a className="fa-link" href="https://www.linkedin.com/in/zacksheppard/">
-        <span className="sr-only">Zack's Linked In</span>
-        <FontAwesomeIcon icon={faLinkedinIn} />
-      </a>
-      <a className="fa-link" href="https://www.github.com/zackzachariah">
-        <span className="sr-only">Zack's Github</span>
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-      <a className="fa-link" href="https://www.twitter.com/zackzachariah">
-        <span className="sr-only">Zack's Twitter</span>
-        <FontAwesomeIcon icon={faTwitter} />
-      </a>
-      <a className="fa-link" href="https://www.instagram.com/zackzachariah">
-        <span className="sr-only">Zack's Instagram</span>
-        <FontAwesomeIcon icon={faInstagram} />
-      </a>
+      <div>
+        <a className="fa-link" href="mailto:contact@zacksheppard.com">
+          <span className="sr-only">Email Zack</span>
+          <FontAwesomeIcon icon={faEnvelopeOpenText} />
+        </a>
+        <a className="fa-link" href="https://www.linkedin.com/in/zacksheppard/">
+          <span className="sr-only">Zack's Linked In</span>
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+        <a className="fa-link" href="https://www.github.com/zackzachariah">
+          <span className="sr-only">Zack's Github</span>
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
+      <div>
+        <a className="fa-link" href="https://www.twitter.com/zackzachariah">
+          <span className="sr-only">Zack's Twitter</span>
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a className="fa-link" href="https://www.instagram.com/zackzachariah">
+          <span className="sr-only">Zack's Instagram</span>
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+      </div>
     </div>
   </div>
 );
