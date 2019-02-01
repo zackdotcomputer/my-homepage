da589aeb48959bf8716db3ca123d250ddefee328
zackzachariah <zack@zacksheppard.com>
2019-02-01 16:17:46 +0000

    Fix warnings in head tags

--- js/layout/HeadTags.tsx
+++ js/layout/HeadTags.tsx
@@ -56,117 +56,68 @@ export default class HeadTags extends React.PureComponent<Props> {
     return "";
   }
 
-  _twitterTags(): JSX.Element[] {
-    let results = [
-      <meta name="twitter:title" content={this._getPageTitle()} />
-    ];
-
-    if (this.props.imageUrlForTwitter) {
-      results.push(<meta name="twitter:card" content="summary_large_image" />);
-      results.push(
-        <meta
-          name="twitter:image:src"
-          content={this.props.imageUrlForTwitter}
-        />
-      );
-    } else {
-      results.push(<meta name="twitter:card" content="summary" />);
-    }
-
-    if (this.props.twitterHandle) {
-      results.push(
-        <meta name="twitter:site" content={this.props.twitterHandle} />
-      );
-      results.push(
-        <meta name="twitter:creator" content={this.props.twitterHandle} />
-      );
-    }
-
-    if (this.props.description) {
-      results.push(
-        <meta name="twitter:description" content={this.props.description} />
-      );
-    }
-
-    return results;
-  }
-
-  _fbTags(): JSX.Element[] {
-    let results = [<meta property="og:type" content="article" />];
-
-    if (this.props.pageTitle) {
-      results.push(<meta property="og:title" content={this.props.pageTitle} />);
-    }
-
-    if (this.props.siteTitle) {
-      results.push(
-        <meta property="og:site_name" content={this.props.siteTitle} />
-      );
-    }
-
-    if (this.props.canonicalUrl) {
-      results.push(
-        <meta property="og:url" content={this.props.canonicalUrl} />
-      );
-    }
-
-    if (this.props.description) {
-      results.push(
-        <meta property="og:description" content={this.props.description} />
-      );
-    }
-
-    if (this.props.imageUrlForFacebook) {
-      results.push(
-        <meta property="og:image" content={this.props.imageUrlForFacebook} />
-      );
-    }
-
-    return results;
-  }
-
-  _htmlTags(): JSX.Element[] {
-    let results = [
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />,
-      <title>{this._getPageTitle()}</title>
-    ];
-
-    if (this.props.disallowRobots === true) {
-      results.push(<meta name="robots" content="noindex" />);
-    }
-
-    if (this.props.faviconUrl) {
-      results.push(
-        <link rel="icon" type="image/png" href={this.props.faviconUrl} />
-      );
-    }
-
-    if (this.props.manifestUrl) {
-      results.push(<link rel="manifest" href={this.props.manifestUrl} />);
-    }
-
-    if (this.props.description) {
-      results.push(
-        <meta name="description" content={this.props.description} />
-      );
-    }
-
-    if (this.props.canonicalUrl) {
-      results.push(<link rel="canonical" href={this.props.canonicalUrl} />);
-    }
-
-    return results;
-  }
-
   render() {
     return (
       <Head>
-        {this._htmlTags()}
-        {this._fbTags()}
-        {this._twitterTags()}
+        {/* HTML meta Tags */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>{this._getPageTitle()}</title>
+        {this.props.disallowRobots && <meta name="robots" content="noindex" />}
+        {this.props.faviconUrl && (
+          <link rel="icon" type="image/png" href={this.props.faviconUrl} />
+        )}
+        {this.props.manifestUrl && (
+          <link rel="manifest" href={this.props.manifestUrl} />
+        )}
+        {this.props.description && (
+          <meta name="description" content={this.props.description} />
+        )}
+        {this.props.canonicalUrl && (
+          <link rel="canonical" href={this.props.canonicalUrl} />
+        )}
+        {/* Facebook OpenGraph Tags */}
+        <meta property="og:type" content="article" />
+        {this.props.pageTitle && (
+          <meta property="og:title" content={this.props.pageTitle} />
+        )}
+        {this.props.siteTitle && (
+          <meta property="og:site_name" content={this.props.siteTitle} />
+        )}
+        {this.props.canonicalUrl && (
+          <meta property="og:url" content={this.props.canonicalUrl} />
+        )}
+        {this.props.description && (
+          <meta property="og:description" content={this.props.description} />
+        )}
+        {this.props.imageUrlForFacebook && (
+          <meta property="og:image" content={this.props.imageUrlForFacebook} />
+        )}
+        {/* Twitter Card Tags */}
+        <meta name="twitter:title" content={this._getPageTitle()} />
+        {this.props.imageUrlForTwitter && (
+          <meta name="twitter:card" content="summary_large_image" />
+        )}
+        {this.props.imageUrlForTwitter && (
+          <meta
+            name="twitter:image:src"
+            content={this.props.imageUrlForTwitter}
+          />
+        )}
+        {!this.props.imageUrlForTwitter && (
+          <meta name="twitter:card" content="summary" />
+        )}
+        {this.props.twitterHandle && (
+          <meta name="twitter:site" content={this.props.twitterHandle} />
+        )}
+        {this.props.twitterHandle && (
+          <meta name="twitter:creator" content={this.props.twitterHandle} />
+        )}
+        {this.props.description && (
+          <meta name="twitter:description" content={this.props.description} />
+        )}
       </Head>
     );
   }
