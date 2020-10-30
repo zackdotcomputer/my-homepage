13eed33fab5e4db989c9fc204cf5c702df38f549
Zack Sheppard <zack@zacksheppard.com>
2020-10-29 20:16:05 -0400

    Try out the next/image optimizer

--- js/components/BackToTop.tsx
+++ js/components/BackToTop.tsx
@@ -1,20 +1,23 @@
 // Created 1/2019 by Zack Sheppard (zacksheppard.com)
 
 import React from "react";
+import Image from "next/image";
 
 const BackToTop = () => {
   const backToTop = () => {
     window.scrollTo({
       behavior: "smooth",
-      top: 0
+      top: 0,
     });
   };
 
   return (
     <div className="back-to-top">
       <a onClick={backToTop}>
-        <img
+        <Image
           className="cta-arrow"
+          width="34"
+          height="12"
           src="/static/img/arrow-up.png"
           alt="arrow pointing up"
         />
