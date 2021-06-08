8f3619b9e5c0e329b406899a352c167c220f323a
Zack Sheppard <zack@zacksheppard.com>
2021-06-08 12:41:20 +0100

    Upgrade to webpack 5

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 module.exports = {
   target: "serverless",
 
-  webpack: (config) => {
+  future: {
+    webpack5: true
+  },
+
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack"
-        }
-      ]
+      use: ["@svgr/webpack"]
     });
 
-    // Important: return the modified config
     return config;
   }
 };
