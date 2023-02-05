7320ef530c1dde9a94bdee093e5dfdd6e11674ac
Zack Sheppard <zack@zacksheppard.com>
2023-02-05 00:18:17 +0000

    Trying to force update of the next-on-netlify package

--- src/util/env.ts
+++
@@ -1,5 +0,0 @@
-// Used with permission under the MIT license from ZEIT's Next.js project.
-// See license information at https://github.com/zeit/next.js
-
-// eslint-disable-next-line no-underscore-dangle
-export default typeof window !== "undefined" ? (window as any).__ENV__ : process.env;
