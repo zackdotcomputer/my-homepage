a7b17d8578b1a266932c3c49d0d81d2fadcc4752
Zack Sheppard <zack@zacksheppard.com>
2023-02-26 17:28:25 +0000

    Move from plausible to umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import Head from "next/head";
+import Script from "next/script";
 import "../../styles/index.css";
 import Conversation from "../layout/Conversation";
 import NormcoreLayout from "../layout/Normcore/NormcoreLayout";
@@ -7,41 +7,23 @@ import NormcoreLayout from "../layout/Normcore/NormcoreLayout";
 function MyApp({ Component, pageProps, router }: AppProps) {
   const isNormcore = router.pathname.startsWith("/normcore");
 
-  if (isNormcore) {
-    return (
-      <>
-        <Head>
-          {process.env.NODE_ENV !== "development" && (
-            <script
-              async
-              defer
-              data-domain="zack.computer"
-              src="https://plausible.io/js/plausible.js"
-            />
-          )}
-        </Head>
+  return (
+    <>
+      {isNormcore ? (
         <NormcoreLayout>
           <Component {...pageProps} />
         </NormcoreLayout>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Head>
-          {process.env.NODE_ENV !== "development" && (
-            <script
-              async
-              defer
-              data-domain="zack.computer"
-              src="https://plausible.io/js/plausible.js"
-            />
-          )}
-        </Head>
+      ) : (
         <Conversation path={router.pathname} contents={<Component {...pageProps} />} />
-      </>
-    );
-  }
+      )}
+      <Script
+        async
+        defer
+        data-website-id="7673694d-cf72-4c29-bea7-fcbbf9dd4e4a"
+        src="https://analytics.zack.computer/umami.js"
+      />
+    </>
+  );
 }
 
 export default MyApp;
